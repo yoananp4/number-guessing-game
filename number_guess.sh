fee8cc7f703441626d68a8158e3891b15fda870e
yoananp4 <yoananabilahp@student.esaunggul.ac.id>
2026-01-16 08:02:20 -0500

    fix: correct output messages and guess counting

--- number_guess.sh
+++ number_guess.sh
@@ -9,7 +9,7 @@ USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$US
 
 if [[ -z $USER_DATA ]]
 then
-  echo "Welcome, $USERNAME! It looks like it your first time here."
+  echo "Welcome, $USERNAME! It looks like is your first time here."
 
   INSERT_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
   GAMES_PLAYED=0
@@ -27,12 +27,13 @@ echo "Guess the secret number between 1 and 1000:"
 
 while true; do
   read GUESS
-  ((NUM_GUESSES++))
 
   if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
     continue
   fi
+
+  ((NUM_GUESSES++))
 
   if (( GUESS < SECRET_NUMBER )); then
     echo "It's higher than that, guess again:"
@@ -42,7 +43,7 @@ while true; do
    echo "You guessed it in $NUM_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
 
    GAMES_PLAYED=$((GAMES_PLAYED + 1))
-   if [[ $BEST_GAME -eq 0 || $NUM_GUESSES -lt $BEST_GAME ]]; then
+   if [[ -z $BEST_GAME || $NUM_GUESSES -lt $BEST_GAME ]]; then
       BEST_GAME=$NUM_GUESSES
     fi
     $PSQL "UPDATE users SET games_played=$GAMES_PLAYED, best_game=$BEST_GAME WHERE username='$USERNAME';"
