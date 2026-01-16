690a5b381d5cbb74fa12ceeb231295bc48ee6c8a
yoananp4 <yoananabilahp@student.esaunggul.ac.id>
2026-01-16 10:29:07 -0500

    fix: correct output messages and guess counting

--- number_guess.sh
+++ number_guess.sh
@@ -5,11 +5,11 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
 
-USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME';")
-GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM user INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
-BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM useres INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+USER_DATA=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username ='$USERNAME'")
+BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
 
-if [[ $USER_DATA ]]; 
+if [[ -z $USER_DATA ]]
   then
     INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
     echo "Welcome, $USERNAME! It looks like this is your first time here."
@@ -17,7 +17,7 @@ else
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-RANDOM_NUM=$((1 + RANDOM % 1000))
+RANDOM_NUM=$((1 + $RANDOM % 1000))
 GUESS=1
 
 echo "Guess the secret number between 1 and 1000:"
@@ -51,12 +51,6 @@ if [[ $GUESS == 1 ]]
     echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
 fi
 
-if [[ $BEST_GAME -eq 0 || $NUM_GUESSES -lt $BEST_GAME ]]; then
-      BEST_GAME=$NUM_GUESSES
-    fi
-    $PSQL "UPDATE users SET games_played=$GAMES_PLAYED, best_game=$BEST_GAME WHERE username='$USERNAME';"
-    break
-  fi
-done
-
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+INSERT_GAME=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES($GUESS, $USER_ID)")
 # commit 1: add database connection
